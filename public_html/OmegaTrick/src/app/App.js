c042173a6fbb8727b0f527cd691dce9efc55b3ca
Kazuhiro Kotsutsumi <kotsutsumi@xenophy.com>
2010-03-25 10:23:36 +0900

    アプリケーションクラス追加

---
+++ public_html/OmegaTrick/src/app/App.js
@@ -0,0 +1,33 @@
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+// {{{ Namespace
+
+Ext.ns(
+    'Ext.trick',
+    'Ext.trick.app'
+);
+
+// }}}
+// {{{ Ext.trick.app.App
+
+/**
+ * Ext.trick.app.App Class
+ *
+ * @author  Kazuhiro Kotsutsumi <kotsutsumi@xenophy.com>
+ * @version 1.0
+ */
+Ext.trick.app.App = Ext.extend(Ext.util.Observable, {
+
+
+});
+
+// }}}
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * End:
+ */
+
