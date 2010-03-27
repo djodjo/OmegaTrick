031a6c241289fb9e2860c76492cb069c1af3a802
Kazuhiro Kotsutsumi <kotsutsumi@xenophy.com>
2010-03-27 12:38:29 +0900

    Ext JSディレクトリをテンプレート変数へ格納するようにした。 controller/.OmegaTrickEnv.php で、設定しています。 今後、テンプレート変数への初期設定は、このスーパーアクション コントローラーで行います。

---
+++ controllers/.OmegaTrickEnv.php
@@ -0,0 +1,15 @@
+<?php
+
+class OmegaTrickEnv extends xFrameworkPX_Controller_Action
+{
+
+    public function execute()
+    {
+
+        // Ext JS ディレクトリ設定
+        $this->set('extjsdir', 'ext-3.2-rc'); 
+    
+    }
+
+
+}
