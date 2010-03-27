9ed7996ab9b152d1566123e2c857a073d2899593
Kazuhiro Kotsutsumi <kotsutsumi@xenophy.com>
2010-03-27 09:26:43 +0900

    xFrameworkPX(2010/03/27時点のTrunk)を追加

---
+++ library/xFrameworkPX/Config/Exception.php
@@ -0,0 +1,44 @@
+<?php
+
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/**
+ * xFrameworkPX_Config_Exception Class File
+ *
+ * PHP versions 5
+ *
+ * @category   xFrameworkPX
+ * @package    xFrameworkPX_Config
+ * @author     Kazuhiro Kotsutsumi <kotsutsumi@xenophy.com>
+ * @copyright  Copyright (c) 2006-2010 Xenophy.CO.,LTD All rights Reserved.
+ * @license    http://www.opensource.org/licenses/mit-license.html MIT License
+ * @version    SVN $Id$
+ */
+
+// {{{ xFrameworkPX_Config_Exception
+
+/**
+ * xFrameworkPX_Config_Exception Class
+ *
+ * @category   xFrameworkPX
+ * @package    xFrameworkPX_Config
+ * @author     Kazuhiro Kotsutsumi <kotsutsumi@xenophy.com>
+ * @copyright  Copyright (c) 2006-2010 Xenophy.CO.,LTD All rights Reserved.
+ * @license    http://www.opensource.org/licenses/mit-license.html MIT License
+ * @version    Release: 3.5.0
+ * @link       http://www.xframeworkpx.com/api/?class=xFrameworkPX_Config_Exception
+ */
+class xFrameworkPX_Config_Exception extends xFrameworkPX_Exception
+{
+
+}
+
+// }}}
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * End:
+ */
