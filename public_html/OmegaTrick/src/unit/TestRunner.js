cefdc7fa6baecc8b29881b3a4e9f208942bf652d
Kazuhiro Kotsutsumi <kotsutsumi@xenophy.com>
2010-03-28 22:08:01 +0900

    TestRunnerクラスと、TestSuiteクラス追加（空だけど）

---
+++ public_html/OmegaTrick/src/unit/TestRunner.js
@@ -0,0 +1,38 @@
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+// {{{ Namsespace
+
+Ext.ns(
+    'Ext.trick',
+    'Ext.trick.unit'
+);
+
+// }}}
+// {{{ Ext.trick.unit.TestRunner
+
+/**
+ * Ext.trick.unit.TestRunner
+ *
+ * テストランナークラス
+ *
+ * @author  Kazuhiro Kotsutsumi <kotsutsumi@xenophy.com>
+ * @version 1.0
+ */
+Ext.trick.unit.TestRunner = function() {
+
+    var me = this;
+
+
+
+};
+
+// }}}
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * End:
+ */
+
---
+++ public_html/OmegaTrick/src/unit/TestSuite.js
@@ -0,0 +1,38 @@
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+// {{{ Namsespace
+
+Ext.ns(
+    'Ext.trick',
+    'Ext.trick.unit'
+);
+
+// }}}
+// {{{ Ext.trick.unit.TestSuite
+
+/**
+ * Ext.trick.unit.TestSuite
+ *
+ * テストスイートクラス
+ *
+ * @author  Kazuhiro Kotsutsumi <kotsutsumi@xenophy.com>
+ * @version 1.0
+ */
+Ext.trick.unit.TestSuite = function() {
+
+    var me = this;
+
+
+
+};
+
+// }}}
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * End:
+ */
+
