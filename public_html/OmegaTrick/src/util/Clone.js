f964814d7d3e88ab74ea23a56b07686866b859dd
Kazuhiro Kotsutsumi <kotsutsumi@xenophy.com>
2010-03-28 20:31:35 +0900

    Ext.trick.util.clone 作り始めた。

---
+++ public_html/OmegaTrick/src/util/Clone.js
@@ -0,0 +1,61 @@
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+// {{{ Namespace
+
+Ext.ns('Ext.trick', 'Ext.trick.util');
+
+// }}}
+// {{{ Ext.trick.util.clone
+
+/**
+ * Ext.trick.util.clone
+ *
+ * オブジェクトクローン生成関数
+ *
+ * @author  Kazuhiro Kotsutsumi <kotsutsumi@xenophy.com>
+ * @version 1.0
+ */
+Ext.trick.util.clone = function(o) {
+
+    // オブジェクトでないか、偽判定の場合は、引数をそのまま返却する
+    if (!o || 'object' !== typeof o) {
+        return o;
+    }
+
+    // 関数オブジェクトの場合、cloneメソッドの戻り値を返却する
+    if ('function' === typeof o.clone) {
+        return o.clone();
+    }
+
+    // 引数が配列の場合、空の配列を、それ以外はオブジェクトを生成
+    var c = '[object Array]' === Object.prototype.toString.call(o) ? [] : {};
+    var p, v;
+
+    // 再帰処理で内部のデータを複製
+    for(p in o) {
+        if(o.hasOwnProperty(p)) {
+            v = o[p];
+            if(v && 'object' === typeof v) {
+                // Ext.trick.util.cloneの再帰コール
+                c[p] = Ext.trick.util.clone(v);
+            }
+            else {
+                c[p] = v;
+            }
+        }
+    }
+
+    // 複製したオブジェクトを返却
+    return c;
+};
+
+// }}}
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * End:
+ */
+
