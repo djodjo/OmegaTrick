d3dd1d7f76a3fb66da9839f300d13618cf006634
Kazuhiro Kotsutsumi <kotsutsumi@xenophy.com>
2010-03-28 20:38:37 +0900

    Array.prototype.clone Object.prototype.clone
    に、Ext.trick.util.cloneを設定するようにするコード追加

---
+++ public_html/OmegaTrick/src/core/Array.js
@@ -0,0 +1,20 @@
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+// {{{ Array.prototype.clone
+
+if(!Array.prototype.clone) {
+
+    // Array.prototype.cloneにExt.trick.util.cloneを設定
+    Array.prototype.clone = Ext.trick.util.clone;
+}
+
+// }}}
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * End:
+ */
+
---
+++ public_html/OmegaTrick/src/core/Object.js
@@ -0,0 +1,20 @@
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+// {{{ Object.prototype.clone
+
+if(Object.prototype.clone) {
+
+    // Object.prototype.cloneにExt.trick.util.cloneを設定
+    Object.prototype.clone = Ext.trick.util.clone;
+}
+
+// }}}
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * End:
+ */
+
