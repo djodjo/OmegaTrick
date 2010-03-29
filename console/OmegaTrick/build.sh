5eb8a35ed25d17371a699b9e50e8764f192a86ee
Kazuhiro Kotsutsumi <kotsutsumi@xenophy.com>
2010-03-30 08:12:56 +0900

    とりあえず、ビルドしてOmegaTrick-all.jsの生成はできた。

---
+++ console/OmegaTrick/build.sh
@@ -0,0 +1,16 @@
+
+#./app.sh;
+#./core.sh;
+#./draw.sh;
+#./locale.sh;
+#./plugins.sh;
+#./util.sh;
+#./widgets.sh;
+
+cat ../../public_html/OmegaTrick/pkgs/app-min.js \
+    ../../public_html/OmegaTrick/pkgs/draw-min.js \
+    ../../public_html/OmegaTrick/pkgs/plugins-min.js \
+    ../../public_html/OmegaTrick/pkgs/util-min.js \
+    ../../public_html/OmegaTrick/pkgs/widgets-min.js \
+    ../../public_html/OmegaTrick/pkgs/core-min.js > ../../public_html/OmegaTrick/OmegaTrick-all.js
+
