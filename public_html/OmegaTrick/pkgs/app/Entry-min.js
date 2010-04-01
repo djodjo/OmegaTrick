7407af0804c5b8bf61f0e3d7316cd6a16c0d02d2
Kazuhiro Kotsutsumi <kotsutsumi@xenophy.com>
2010-04-01 13:34:41 +0900

    Ext.trick.app.Entryが参加
    だけど、ちょっと結合変かも。
    とりあえず、個別に読み込んで動作確認しながら作業中

---
+++ public_html/OmegaTrick/pkgs/app/Entry-min.js
@@ -0,0 +1,1 @@
+Ext.trick.app.Entry=function(){var me=this,apps={};return{add:function(app){},remove:function(id){},boot:function(){}}}();Ext.onReady(Ext.trick.app.Entry.boot);
---
+++ webapp/OmegaTrick/examples/app/simple.js
@@ -0,0 +1,17 @@
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+SimpleApp = Ext.extend(Ext.trick.app.App, {
+        
+        
+});
+
+Ext.trick.app.Entry.add(SimpleApp);
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * End:
+ */
+
