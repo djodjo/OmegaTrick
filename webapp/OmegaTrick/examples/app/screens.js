c06cb8e706f2fdb8603e0d5461af11d8d4f164f9
Kazuhiro Kotsutsumi <kotsutsumi@xenophy.com>
2010-04-02 22:47:15 +0900

    複数スクリーンアプリケーションサンプル作成開始

---
+++ webapp/OmegaTrick/examples/app/screens.js
@@ -0,0 +1,25 @@
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+Ext.onTrick({
+
+    // {{{ start
+
+    /**
+     * 開始メソッド
+     */
+    start : function() {
+        alert("アプリケーション開始"); 
+    }
+    
+    // }}}
+
+});
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * End:
+ */
+
