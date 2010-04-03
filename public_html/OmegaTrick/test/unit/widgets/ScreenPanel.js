e7447587101329da2580e3db44d64c3a7efa547a
Kazuhiro Kotsutsumi <kotsutsumi@xenophy.com>
2010-04-04 04:59:01 +0900

    Ext.trick.widgets.ScreenPanel ユニットテストの枠作成完了

---
+++ public_html/OmegaTrick/test/unit/widgets/ScreenPanel.js
@@ -0,0 +1,46 @@
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+// {{{ Namespace
+
+Ext.ns(
+    'Ext.trick',
+    'Ext.trick.test',
+    'Ext.trick.test.unit',
+    'Ext.trick.test.unit.widgets'
+);
+
+// }}}
+// {{{ Ext.trick.test.unit.widgets.ScreenPanel
+
+/**
+ * Ext.trick.test.unit.widgets.ScreenPanelオブジェクト
+ *
+ */
+Ext.trick.test.unit.widgets.ScreenPanel = new Ext.trick.unit.TestCase({
+
+    // TestCase名
+    name: "Ext.trick.test.unit.widgets.ScreenPanel",
+
+    // {{{ testScreenPanelObject
+
+    /**
+     * オブジェクトクローンテスト
+     */
+    testScreenPanelObject : function() {
+    
+        var me = this;
+       
+    }
+
+    // }}}
+
+});
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * End:
+ */
+
