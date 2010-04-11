c0d5c3bf2bc254928403f1cc1ad1c8d26bac2cf8
Kazuhiro Kotsutsumi <kotsutsumi@xenophy.com>
2010-04-12 02:10:40 +0900

    認証処理の実装を行いました。
    まだDDLの設置（コメント内）を行っていません。
    また、useTableの指定も行っていません。

--- modules/OmegaTrick/Auth.php
+++ modules/OmegaTrick/Auth.php
@@ -1,9 +1,132 @@
 <?php
 
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/**
+ * OmegaTrick_Auth Class File
+ *
+ * PHP versions 5
+ *
+ * @category   OmegaTrick
+ * @package    OmegaTrick_Auth
+ * @author     Kazuhiro Kotsutsumi <kotsutsumi@xenophy.com>
+ * @copyright  Copyright (c) 2006-2010 Xenophy.CO.,LTD All rights Reserved.
+ * @license    http://www.gnu.org/licenses/gpl-3.0.html GPL v3
+ */
+
+// {{{ SpreadOffice_Auth
+
+/**
+ * OmegaTrick_Auth Class
+ *
+ * このクラスでは、MySQLを利用したユーザ管理を前提に実装されています。
+ * 利用するDDLは次の通りです。
+ *
+ * [DDL]
+ *
+ *
+ *
+ * 必要に応じて、OmegaTrick_Authクラスを継承しメソッドをオーバライドしてくださ
+ * い。
+ *
+ * @category   OmegaTrick
+ * @package    OmegaTrick_Auth
+ * @author     Kazuhiro Kotsutsumi <kotsutsumi@xenophy.com>
+ * @copyright  Copyright (c) 2006-2010 Xenophy.CO.,LTD All rights Reserved.
+ * @license    http://www.gnu.org/licenses/gpl-3.0.html GPL v3
+ */
 class OmegaTrick_Auth extends xFrameworkPX_Model
 {
+    // {{{ props
+
+    /**
+     * 使用テーブル設定
+     *
+     * @var string
+     */
     public $usetable = false;
 
+    /**
+     * ユーザーセッションキー
+     */
+    public $userSessionKey = 'OMEGATRICK_USER_SESSION_KEY';
+
+    /**
+     * 自動サインイン暗号化キー
+     */
+    public $autoSigninEncryptKey = 'OMEGATRICK_AUTO_SIGNIN_KEY';
+
+    // }}}
+    // {{{ autoSignin
+
+    /**
+     * 自動サインインメソッド
+     */
+    public function autoSignin($key) {
+
+        $key = decrypt(
+            $this->autoSigninEncryptKey,
+            $key
+        );
+
+        $ret = $this->count(
+            array(
+                'where' => array(
+                    'auto_signin_key = :auto_signin_key',
+                ),
+                'bind' => array(
+                    'auto_signin_key' => $key,
+                ),
+            )
+        );
+
+        if ($ret === 1) {
+
+            // ユーザー情報取得
+            $ret = $this->get(
+                'first',
+                array(
+                    'conditions' => array(
+                        'auto_signin_key' => $key,
+                    ),
+                )
+            );
+
+            // 自動ログイン用キー更新
+            $autoSigninKey = md5(uniqid("",1));
+            $this->set(
+                array(
+                    'id' => $ret['id'],
+                    'auto_signin_key' => $autoSigninKey
+                )
+            );
+
+            // セッションへユーザーデータ保存
+            $this->controller->Session->write(
+                'SPREAD_OFFICE_USER',
+                array(
+                    'id' => $ret['id'],
+                    'company' => $ret['company_id'],
+                    'last_name' => $ret['last_name'],
+                    'first_name' => $ret['first_name'],
+                )
+            );
+
+            return array(
+                'success' => true,
+                'auto_signin_key' => encrypt(
+                    $this->autoSigninEncryptKey,
+                    $autoSigninKey
+                )
+            );
+        }
+
+        return array(
+            'success' => false
+        );
+    }
+
+    // }}}
     // {{{ isSignin
 
     /**
@@ -11,8 +134,16 @@ class OmegaTrick_Auth extends xFrameworkPX_Model
      */
     public function isSignin() {
 
-        return false;
+        $ret = $this->controller->Session->read($this->userSessionKey);
 
+        if ($ret) {
+            return array(
+                'first_name' => $ret['first_name'],
+                'last_name' => $ret['last_name']
+            );
+        }
+
+        return false;
     }
 
     // }}}
@@ -23,13 +154,105 @@ class OmegaTrick_Auth extends xFrameworkPX_Model
      */
     public function execute($params) {
 
+        $ret = $this->count(
+            array(
+                'where' => array(
+                    'del = :del',
+                    'email = :email',
+                    'password = :password'
+                ),
+                'bind' => array(
+                    'del' => 0,
+                    'email' => $param->email,
+                    'password' => $param->pass
+                ),
+            )
+        );
 
+        if ($ret === 1) {
 
-        return false;
+            // ユーザー情報取得
+            $ret = $this->get(
+                'first',
+                array(
+                    'conditions' => array(
+                        'email' => $param->email,
+                        'password' => $param->pass
+                    ),
+                )
+            );
 
+            // 自動ログイン用キー更新
+            $autoSigninKey = md5(uniqid("",1));
+            $this->set(
+                array(
+                    'id' => $ret['id'],
+                    'auto_signin_key' => $autoSigninKey
+                )
+            );
+
+            // セッションへユーザーデータ保存
+            $this->controller->Session->write(
+                $this->userSessionKey,
+                array(
+                    'id' => $ret['id'],
+                    'company' => $ret['company_id'],
+                    'last_name' => $ret['last_name'],
+                    'first_name' => $ret['first_name'],
+                )
+            );
+
+            return array(
+                'success' => true,
+                'auto_signin_key' => encrypt(
+                    $this->autoSigninEncryptKey,
+                    $autoSigninKey
+                )
+            );
+        }
+
+        return array(
+            'success' => false
+        );
+    }
+
+    // }}}
+    // {{{ signout
+
+    /**
+     * 認証実行メソッド
+     */
+    public function signout() {
+
+        // セッションへユーザーデータ保存
+        $ret = $this->controller->Session->read($this->userSessionKey);
+
+        // セッションのユーザーデータ破棄
+        $this->controller->Session->remove($this->userSessionKey);
+
+        // 自動ログイン用キー更新
+        $autoSigninKey = md5(uniqid("",1));
+        $this->set(
+            array(
+                'id' => $ret['id'],
+                'auto_signin_key' => $autoSigninKey
+            )
+        );
+
+        return array(
+            'success' => true
+        );
     }
 
     // }}}
 
 }
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * End:
+ */
 
