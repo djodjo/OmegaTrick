beaeaa70fe0918844526d976f860a06f056c2a5b
Kazuhiro Kotsutsumi <kotsutsumi@xenophy.com>
2010-04-14 15:21:50 +0900

    Ext.trick.TrickPanel追加

---
+++ public_html/OmegaTrick/src/widgets/TrickPanel.js
@@ -0,0 +1,45 @@
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+/*jslint evil: true */
+
+// {{{ Ext.trick.TrickPanel
+
+/**
+ * Ext.trick.TrickPanel Class
+ *
+ * @author  Kazuhiro Kotsutsumi <kotsutsumi@xenophy.com>
+ * @version 1.0
+ */
+Ext.trick.TrickPanel = Ext.extend(Ext.Component, {
+
+    // {{{ initComponent
+
+    /**
+     * コンポーネント初期化メソッド
+     *
+     * @return void
+     */
+    initComponent : function() {
+
+        var me = this;
+
+    }
+
+    // }}}
+
+});
+
+// }}}
+// {{{ Register xtype
+
+Ext.reg('trick', Ext.trick.TrickPanel);
+
+// }}}
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * End:
+ */
+
