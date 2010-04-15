7e18769a4e8c7dfc98b80f7f00c5b0825f26106d
Kazuhiro Kotsutsumi <kotsutsumi@xenophy.com>
2010-04-15 11:04:07 +0900

    List Trickファイル追加

---
+++ public_html/OmegaTrick/src/widgets/trick/List.js
@@ -0,0 +1,50 @@
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+// {{{ Namespace
+
+Ext.ns(
+    'Ext.trick',
+    'Ext.trick.trick'
+);
+
+// }}}
+// {{{ Ext.trick.trick.List
+
+/**
+ * Ext.trick.trick.List Class for enhancing
+ *
+ * @author  Kazuhiro Kotsutsumi <kotsutsumi@xenophy.com>
+ * @version 1.0
+ */
+Ext.trick.trick.List = {
+
+    // {{{ tricktrigger
+
+    /**
+     * トリックトリガーメソッド
+     *
+     * 仕掛けの設置を開始します。
+     */
+    tricktrigger : function() {
+
+
+
+
+
+
+    }
+
+    // }}}
+
+};
+
+// }}}
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * End:
+ */
+
