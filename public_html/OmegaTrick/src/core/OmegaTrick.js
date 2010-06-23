6a23f5580f730384853a01b3519b82d97ac8a034
Kazuhiro Kotsutsumi <kotsutsumi@xenophy.com>
2010-06-24 04:47:40 +0900

    OmegaTrickオブジェクト作ることにしました〜。

---
+++ public_html/OmegaTrick/src/core/OmegaTrick.js
@@ -0,0 +1,51 @@
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+// {{{ OmegaTrick
+
+/**
+ * @class OmegaTrick
+ *
+ * OmegaTrick クラス
+ *
+ * @singleton
+ */
+OmegaTrick = {
+
+    /**
+     * バージョン情報
+     * @type String
+     */
+    version : '0.5.0',
+    versionDetail : {
+        major: 0,
+        minor: 5,
+        patch: 0
+    }
+
+};
+
+(function(){
+
+    // OmegaTrickオブジェクト実装
+    Ext.apply(OmegaTrick, {
+
+
+
+    });
+
+})();
+
+// }}}
+// {{{ Namespace
+
+Ext.ns('OmegaTrick.app','OmegaTrick.util');
+
+// }}}
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * End:
+ */
