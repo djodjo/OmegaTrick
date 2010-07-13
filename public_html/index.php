fb4d3b5465cae1e4f9a48bd3ec8b7b9f352b8d62
Kazuhiro Kotsutsumi <kotsutsumi@xenophy.com>
2010-07-13 16:35:25 +0900

    とりあえず、入れ替え完了？かな。

--- public_html/index.php
+++ public_html/index.php
@@ -1,5 +1,14 @@
 <?php
 
+// {{{ PHPエラー出力設定
+
+/**
+ * PHP 5.xでは、下記でよいが、PHP 6.0 移行は、E_STRICTもE_ALLに含まれるため
+ * E_STRICTでのエラー回避もxFramework PX 側で行う必要がある
+ */
+error_reporting(E_ALL);
+
+// }}}
 // {{{ Set Library Path
 
 set_include_path('../library/' . PATH_SEPARATOR . get_include_path());
@@ -15,7 +24,7 @@ include 'xFrameworkPX/Loader/Core.php';
 
 xFrameworkPX_Dispatcher::getInstance()->run(
     array(
-        'DEBUG' => 2
+        'DEBUG' => 1
     )
 );
 
